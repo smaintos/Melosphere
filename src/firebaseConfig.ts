b385668a1bbe6999f0c183f216846977fb8dda30
william <william.mariamon@ynov.com>
2024-03-08 16:36:03 +0100

    mask

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkfIzXKZl0G_KEPR_jxX_0EDDABllD6eQ",
-  authDomain: "melosphere-14f34.firebaseapp.com",
-  projectId: "melosphere-14f34",
-  storageBucket: "melosphere-14f34.appspot.com",
-  messagingSenderId: "982066449373",
-  appId: "1:982066449373:web:30c35d8bdbc59b430a0434"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
